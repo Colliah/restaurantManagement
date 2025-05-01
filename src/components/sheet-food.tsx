77d790d2956817f25f77e63ccaf51299b6dce8da
Colliah <164477540+Colliah@users.noreply.github.com>
2025-05-01 14:48:12 +0700

    update

--- src/components/sheet-food.tsx
+++ src/components/sheet-food.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import {
   Sheet,
   SheetContent,
@@ -10,7 +10,7 @@ import {
 } from "./ui/sheet";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { Form } from "./ui/form";
+import { Form, FormDescription } from "./ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -21,15 +21,15 @@ import {
   FormControl,
   FormMessage,
 } from "./ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Textarea } from "./ui/textarea";
 import { Switch } from "./ui/switch";
+import {
+  FileInput,
+  FileUploader,
+  FileUploaderContent,
+  FileUploaderItem,
+} from "./ui/file-upload";
+import { CloudUpload, Paperclip, X } from "lucide-react";
 
 const formSchema = z.object({
   name: z.string().min(2, {
@@ -52,6 +52,12 @@ const formSchema = z.object({
 });
 
 const SheetFood = () => {
+  const [files, setFiles] = useState<File[] | null>(null);
+  const dropZoneConfig = {
+    maxFiles: 5,
+    maxSize: 1024 * 1024 * 4,
+    multiple: true,
+  };
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -65,6 +71,7 @@ const SheetFood = () => {
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
+    //maybe làm toast ở đây
     console.log(values);
   }
   return (
@@ -77,7 +84,7 @@ const SheetFood = () => {
           <SheetTitle>Food</SheetTitle>
         </SheetHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
             <FormField
               control={form.control}
               name="name"
@@ -158,14 +165,69 @@ const SheetFood = () => {
               name="imageUrl"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Image Url</FormLabel>
+                  <FormLabel>Select File</FormLabel>
                   <FormControl>
-                    <Input {...field} />
+                    <FileUploader
+                      value={files}
+                      onValueChange={setFiles}
+                      dropzoneOptions={dropZoneConfig}
+                      className="relative bg-background rounded-lg p-2"
+                    >
+                      <FileInput
+                        id="fileInput"
+                        className="outline-dashed outline-1 outline-slate-500"
+                      >
+                        <div className="flex items-center justify-center flex-col p-8 w-full ">
+                          <CloudUpload className="text-gray-500 w-10 h-10" />
+                          <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                            <span className="font-semibold">
+                              Click to upload
+                            </span>
+                            &nbsp; or drag and drop
+                          </p>
+                          <p className="text-xs text-gray-500 dark:text-gray-400">
+                            SVG, PNG, JPG or GIF
+                          </p>
+                        </div>
+                      </FileInput>
+                      <FileUploaderContent className="flex-row gap-2 overflow-x-auto w-[300px]">
+                        {files &&
+                          files.length > 0 &&
+                          files.map((file, i) => {
+                            const imageUrl = URL.createObjectURL(file);
+                            return (
+                              <FileUploaderItem key={i} index={i}>
+                                <div className="relative w-20 h-20">
+                                  <img
+                                    src={imageUrl}
+                                    alt={file.name}
+                                    className="w-20 h-20 object-cover rounded-md"
+                                  />
+                                  <button
+                                    type="button"
+                                    className="absolute top-0 right-0 text-white bg-red-500 p-1 rounded-full"
+                                    onClick={() => {
+                                      const newFiles = files.filter(
+                                        (_, index) => index !== i
+                                      );
+                                      setFiles(newFiles);
+                                    }}
+                                  >
+                                    <X className="w-4 h-4" />
+                                  </button>
+                                </div>
+                              </FileUploaderItem>
+                            );
+                          })}
+                      </FileUploaderContent>
+                    </FileUploader>
                   </FormControl>
+                  <FormDescription>Select a file to upload.</FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
             />
+
             <SheetFooter>
               <Button type="submit">Submit</Button>
             </SheetFooter>
