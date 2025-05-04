da4ab1c3d042b59369b0f51746f8072bad249a8c
KawNguyen <khoanguyencool12@gmail.com>
2025-05-04 15:59:24 +0700

    update-1

---
+++ src/hooks/api/use-ingredient.ts
@@ -0,0 +1,9 @@
+import { ingredientsApi } from "@/services/ingredients";
+import { useQuery } from "@tanstack/react-query";
+
+export const useIngredients = () => {
+  return  useQuery({
+    queryKey: ["ingredients"],
+    queryFn: ingredientsApi.findAll,
+  });
+}
